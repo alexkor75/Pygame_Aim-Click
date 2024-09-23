d21e8acb7e2ff61e9fd26924d416cadab5acc691
Aleksandr Vadimovich Korolev <144994839+alexkor75@users.noreply.github.com>
2024-09-23 20:38:38 +0700

    add_const

--- main.py
+++ main.py
@@ -5,7 +5,23 @@ import time
 pygame.init()
 pygame.mixer.init()
 
+# Настройки экрана
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+BACKGROUND_COLOR = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
+# Настройки игры
+GAME_DURATION = 40  # секунд
+TARGET_SCALE = 7  # Коэффициент уменьшения мишени
+
+# Пути к ресурсам
+ICON_PATH = "img/icon.png"
+TARGET_PATH = "img/target.png"
+MUSIC_PATH = "sounds/music.mp3"
+CLICK_SOUND_PATH = "sounds/click.mp3"
+
+# Скорости мишени
+TARGET_SPEEDS = [-3, -2, -1, 1, 2, 3]
 
 
 # Основной игровой цикл
